32918525777558c021b53291804479ef0aac5a73
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-12-19 11:25:34 -0600

    Renamed components to shared

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import NavBar from '../components/NavBar/NavBar';
+import NavBar from '../shared/NavBar/NavBar';
 import styles from './Root.module.css';
 
 function App() {
